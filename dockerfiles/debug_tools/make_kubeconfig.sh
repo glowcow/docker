c9080931c62caeb2d6a0212a01e567891683f35a
Anton Sediuk <a.sediuk@platformaofd.ru>
2023-08-23 15:59:42 +0300

    upd debug-tools

---
+++ dockerfiles/debug_tools/make_kubeconfig.sh
@@ -0,0 +1,15 @@
+APISERVER=https://<YOUR-KUBERNETES-API-SERVER>
+TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+NAMESPACE=default
+CLUSTER_NAME=my-cluster
+USER_NAME=sa-user
+CONTEXT_NAME=my-context
+CA_FILE_PATH=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+
+# Create a temporary kubeconfig file
+KUBECONFIG_PATH=/tmp/kubeconfig
+
+kubectl config --kubeconfig=${KUBECONFIG_PATH} set-cluster ${CLUSTER_NAME} --embed-certs=true --server=${APISERVER} --certificate-authority=${CA_FILE_PATH}
+kubectl config --kubeconfig=${KUBECONFIG_PATH} set-credentials ${USER_NAME} --token=${TOKEN}
+kubectl config --kubeconfig=${KUBECONFIG_PATH} set-context ${CONTEXT_NAME} --cluster=${CLUSTER_NAME} --namespace=${NAMESPACE} --user=${USER_NAME}
+kubectl config --kubeconfig=${KUBECONFIG_PATH} use-context ${CONTEXT_NAME}
